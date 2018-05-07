36fa34ce8124af99b1e1347d4a270b2f62b2de85
Rasmus Malik Thaarup Hoeegh <rmth@sh-ln03.stanford.edu>
2018-05-06 21:30:53 -0700

    Changes made on cluster.

--- generate_h5_files_sherlock.py
+++ generate_h5_files_sherlock.py
@@ -8,28 +8,33 @@ import datetime
 
 rescale_mode = 'soft'
 data_folder = '/scratch/PI/mignot/nsrr/shhs/polysomnography/edfs/shhs1/'
-df = pd.read_csv('IDs.csv')
+df = pd.read_csv('/home/users/rmth/stroke-deep-learning/IDs.csv')
 IDs = np.asarray(df['IDs'])
 group = np.asarray(df['group'])
 channels_to_load = [2, 7]
-output_folder = '/home/users/rmth/processed_shhs_data/'
+#output_folder = '/home/users/rmth/processed_shhs_data/'
+output_folder = '/scratch/users/rmth/processed_shhs_data/'
 
 for counter, ID in enumerate(IDs):
-    print('Load, filtering, organizing: ' + str(ID) + ' (number ' + str(counter+1) + ' of ' + str(len(IDs)) + ').')
-    if counter > 2: continue
-    filename = data_folder + 'shhs1-' + str(ID) + '.EDF'
-    data, filter = utils.load_edf_file(filename,
+    try:
+    	print('Load, filtering, organizing: ' + str(ID) + ' (number ' + str(counter+1) + ' of ' + str(len(IDs)) + ').')
+    
+    	filename = data_folder + 'shhs1-' + str(ID) + '.edf'
+    	print(filename)
+    	data, filter = utils.load_edf_file(filename,
                                        channels_to_load,
                                        epoch_duration=5)
-    if counter == 0:
-        n_chans, n_epochs, n_epoch_samples = data["sigbufs"].shape
-    output_file_name = output_folder + 'shhs1-' + str(ID) + ".hpf5"
-    with h5py.File(output_file_name, "w") as f:
-        x = data['sigbufs']
-        x = utils.rescale(x, data['fs'], rescale_mode)
-        dset = f.create_dataset("x", data=x, chunks=True)
-        f['fs'] = data['fs']
-        f['group'] = int(group[counter])
+    	if counter == 0:
+        	n_chans, n_epochs, n_epoch_samples = data["sigbufs"].shape
+    	output_file_name = output_folder + 'shhs1-' + str(ID) + ".hpf5"
+    	with h5py.File(output_file_name, "w") as f:
+        	x = data['sigbufs']
+        	x = utils.rescale(x, data['fs'], rescale_mode)
+        	dset = f.create_dataset("x", data=x, chunks=True)
+        	f['fs'] = data['fs']
+        	f['group'] = int(group[counter])
+    except: 
+	print('Error happend while processing: {}'.format(str(filename))
 
 print("All files processed.")
 now = datetime.datetime.now()
