b625260f1598c3c797e0690425829a4f42f9c10d
Rasmus Høegh <rasmusmalikhoegh@gmail.com>
2018-05-12 04:11:13 -0700

    Refactor Config to no longer be a class that uses super due to py27 vs py36 issues.

--- config.py
+++ config.py
@@ -7,51 +7,51 @@ import tensorflow as tf # Not unused even if grey - used in load of params.yaml
 
 # Resource: https://hanxiao.github.io/2017/12/21/Use-HParams-and-YAML-to-Better-Manage-Hyperparameters-in-Tensorflow/
 
-class YParams(HParams):
-    def __init__(self, yaml_fn, config_name):
-        super().__init__()
-        with open(yaml_fn) as fp:
-            for k, v in YAML().load(fp)[config_name].items():
-                if isinstance(v, str) and v[0:2] == 'tf':
-                    self.add_hparam(k, eval(v))
-                else:
-                    self.add_hparam(k, v)
+def new_hparam(yaml_fn, config_name):
+    hparam = HParams()
+    with open(yaml_fn) as fp:
+        for k, v in YAML().load(fp)[config_name].items():
+            if isinstance(v, str) and v[0:2] == 'tf':
+                hparam.add_hparam(k, eval(v))
+            else:
+                hparam.add_hparam(k, v)
+    return hparam
 
-class ModelParams(YParams):
-    pass
-
-class ExperimentParams(YParams):
-    pass
-
-class Config(YParams):
+class Config():
     def __init__(self, config_profile, experiment_profile, model_profile, overrides):
-        # Setup basic configuration (storage)
-        super().__init__('config.yaml', config_profile)
+        self.c = config_profile
+        self.e = experiment_profile
+        self.m = model_profile
+        self.o = overrides
+
+    def get_configs(self):
+        hparam = new_hparam('config.yaml', self.c)
 
         # Setup experimental setup paramenters and name model based on profiles
-        self.eparams = ExperimentParams('experiment.yaml', experiment_profile)
-        self.eparams.add_hparam('ckptdir', self.logdir+config_profile+'_'+experiment_profile+'_'+model_profile+'/')
+        hparam.eparams = new_hparam('experiment.yaml', self.e)
+        hparam.eparams.add_hparam('ckptdir', hparam.logdir+self.c+'_'+self.e+'_'+self.m+'/')
 
         # Setup model parameters/hyperparameters and determine data dimensions
-        self.hparams = ModelParams('params.yaml', model_profile)
-        if overrides:
-            self.hparams.parse(overrides)
-            overrides = overrides.replace('_','')
-            overrides = overrides.replace('=','')
-            self.eparams.ckptdir = self.eparams.ckptdir[0:-1] + '_' + overrides
-            print(self.eparams.ckptdir)
-        if not exists(self.logdir):
-            print('Setting up logging directory at: {}.'.format(self.logdir))
-            mkdir(self.logdir)
-        (n_channels, n_epoch_samples, fs) = determine_data_dimensions(self.data_folder)
-        self.hparams.add_hparam('n_channels', n_channels)
-        self.hparams.add_hparam('n_epoch_samples', n_epoch_samples)
-        self.hparams.add_hparam('n_sub_epoch_samples', n_epoch_samples // self.hparams.time_steps)
-        self.hparams.add_hparam('fs', fs)
+        hparam.hparams = new_hparam('params.yaml', self.m)
+        if self.o:
+            hparam.hparams.parse(self.o)
+            self.o = self.o.replace('_','')
+            self.o = self.o.replace('=','')
+            hparam.eparams.ckptdir = hparam.eparams.ckptdir[0:-1] + '_' + self.o
+            #print(self.eparams.ckptdir)
+        if not exists(hparam.logdir):
+            print('Setting up logging directory at: {}.'.format(hparam.logdir))
+            mkdir(hparam.logdir)
+        (n_channels, n_epoch_samples, fs) = determine_data_dimensions(hparam.data_folder)
+        hparam.hparams.add_hparam('n_channels', n_channels)
+        hparam.hparams.add_hparam('n_epoch_samples', n_epoch_samples)
+        hparam.hparams.add_hparam('n_sub_epoch_samples', n_epoch_samples // hparam.hparams.time_steps)
+        hparam.hparams.add_hparam('fs', fs)
 
-        self.add_hparam('n_channels', n_channels)
-        self.add_hparam('n_epoch_samples', n_epoch_samples)
-        self.add_hparam('fs', fs)
+        hparam.add_hparam('n_channels', n_channels)
+        hparam.add_hparam('n_epoch_samples', n_epoch_samples)
+        hparam.add_hparam('fs', fs)
+        return hparam
         #Override profile parameters if any are passed
 
 
