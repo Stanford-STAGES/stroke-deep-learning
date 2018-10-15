4de6a4b24ae9784cc3f5d50d8cef4319d065c011
Rasmus Hoeegh <rmth@dtu.dk>
2018-10-15 11:15:24 +0200

    Cleanup bash scripts, and start process of splitting notebooks.

--- bash/submit_jobs-screen.sh
+++ bash/submit_jobs-screen.sh
@@ -3,9 +3,5 @@
 FILE=$1
 
 while read -ru 3 LINE; do
-    #echo "$LINE"
-    #IFS=' '; arrIN=($LINE); unset IFS;
-    #echo ${arrIN[5]}${arrIN[7]//'='}
-    #sbatch train.sh  "$LINE" "${arrIN[5]}${arrIN[7]//'='}"
     sbatch train-screen.sh  "$LINE"
 done 3< "$FILE"
--- bash/submit_jobs_inference.sh
+++ bash/submit_jobs_inference.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
 FILE=$1
-
 while read -ru 3 LINE; do
-    #echo "$LINE"
-    #IFS=' '; arrIN=($LINE); unset IFS;
-    #echo ${arrIN[5]}${arrIN[7]//'='}
-    #sbatch train.sh  "$LINE" "${arrIN[5]}${arrIN[7]//'='}"
     sbatch inference.sh  "$LINE"
 done 3< "$FILE"
